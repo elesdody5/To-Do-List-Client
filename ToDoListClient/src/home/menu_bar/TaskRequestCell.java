3d185a3d1397b6314383594e3f206f0f8384e8fb
ghadeerelmahdy96 <GHADEER.mohamed1450@el-eng.menofia.edu.eg>
2020-01-09 12:48:14 +0200

    Notification version 3 - Client

--- ToDoListClient/src/home/menu_bar/TaskRequestCell.java
+++ ToDoListClient/src/home/menu_bar/TaskRequestCell.java
@@ -11,11 +11,9 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.beans.property.BooleanProperty;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.control.ContextMenu;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.layout.Background;
@@ -23,45 +21,43 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
 /**
  *
  * @author Aml Sakr
  */
-public class ListRequestCell extends ListCell<Notifications> {
+public class TaskRequestCell extends ListCell<Notifications> {
+
     @Override
     protected void updateItem(Notifications not, boolean empty) {
         super.updateItem(not, empty);
         Button accept = new Button();
         Button reject = new Button();
+        accept.setVisible(false);
+        reject.setVisible(false);
         accept.setTextFill(Paint.valueOf("white"));
         accept.setBackground(new Background(new BackgroundFill(Paint.valueOf("#6F4CBB"), new CornerRadii(5), Insets.EMPTY)));
         reject.setTextFill(Paint.valueOf("white"));
         reject.setBackground(new Background(new BackgroundFill(Paint.valueOf("grey"), new CornerRadii(5), Insets.EMPTY)));
         Label txt = new Label();
-        //txt.wrapTextProperty(BooleanProperty.booleanProperty());
-        txt.setTextFill(Paint.valueOf("white"));
-         accept.setVisible(false);
-         reject.setVisible(false);
         if (not != null) {
             if (not.getStatus() == null) {
                 setBackground(new Background(new BackgroundFill(Paint.valueOf("#6B4D22"), new CornerRadii(5), Insets.EMPTY)));
-                txt.setText(not.getFromUserId() + " sends you a request to Collaborate in List : " + not.getData());
+                txt.setText(not.getFromUserId() + " Assigns you to Task : " + not.getData());
                 accept.setText("Accept");
                 reject.setText("Reject");
-                 accept.setVisible(true);
+                accept.setVisible(true);
                 reject.setVisible(true);
             } else {
-                setBackground(new Background(new BackgroundFill(Paint.valueOf("#6F4CBB"), new CornerRadii(5), Insets.EMPTY)));
+                setBackground(new Background(new BackgroundFill(Paint.valueOf("#6B4D22"), new CornerRadii(5), Insets.EMPTY)));
                 accept.setVisible(false);
                 reject.setVisible(false);
                 if (not.getStatus() == "1") {
-                    txt.setText("you accepted a Collaboration with " + not.getFromUserId() + " in List : " + not.getData());
+                    txt.setText("you accepted to work with " + not.getFromUserId() + " in Task : " + not.getData());
                 }
                 if (not.getStatus() == "0") {
-                    txt.setText("you rejected a Collaboration with " + not.getFromUserId() + " in List : " + not.getData());
+                    txt.setText("you rejected to work with " + not.getFromUserId() + " in Task : " + not.getData());
                 }
             }
         }
